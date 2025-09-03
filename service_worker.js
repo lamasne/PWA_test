d8a4f9a36112f7fc293f4bd32f827a9935145f6d
lamasne <neillamas@gmail.com>
2025-09-03 16:21:34 +0200

    initial commit

---
+++ service_worker.js
@@ -0,0 +1,6 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.5.4/workbox-sw.js');
+
+workbox.routing.registerRoute(
+  ({request}) => request.destination === 'document',
+  new workbox.strategies.NetworkFirst()
+);
